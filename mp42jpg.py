142bd0bda147b5d80305aeef9508c6047fc5b085
13maron <111685029+13maron@users.noreply.github.com>
2022-08-21 20:36:52 +0900

    Create mp42jpg.py

---
+++ mp42jpg.py
@@ -0,0 +1,14 @@
+# coding: utf-8 (maybe)
+
+import cv2
+import os
+import sys
+
+def conv(path="Bad Apple!!.mp4", frames=6571):
+	capt = cv2.VideoCapture(path)
+	if capt.isOpened():
+		os.makedirs("./images/", exist_ok=True)
+		digit = len(frames)
+		for i in range(digit):
+			suc, imgArray = capt.read()
+			cv2.imwrite('{}{}{}'.format("./images/", str(i).zfill(digit), ".jpg"), imgArray)
